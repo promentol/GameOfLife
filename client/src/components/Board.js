dfdd85f6341488dc51c7a7bbb274e5aa79e48106
Narek H <promentol@gmail.com>
2018-11-06 19:58:52 +0400

    Add Display Board

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import io from 'socket.io-client';
 
 class Board extends Component {
-    renderCell = () => {
-
+    generateArray = (N) => {
+        return Array.apply(null, {length: N}).map(Number.call, Number)
     }
     render() {
+        const heightArray = this.generateArray(this.props.dimension.height)
+        const widthArray = this.generateArray(this.props.dimension.width)
         return (
             <div>
-                {this.props.dimension.height} x {this.props.dimension.width}
+                <table className="game">
+                <tbody>
+                    {heightArray.map((x) => {
+                        return (
+                            <tr key={x}>
+                                {widthArray.map((y) => {
+                                    return <td onClick={()=>this.placePoint(x, y)} key={y}></td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+                </table>
             </div>
         )
     }
