581ed91fca8337ea7497118119101f5a2aadbe7a
Narek H <promentol@gmail.com>
2018-11-06 15:28:57 +0400

    improvements

--- lua/iterate.lua
+++ lua/iterate.lua
@@ -1,4 +1,6 @@
-local prefix = KEYS[1]
+local table = KEYS[1]
+local queue = KEYS[2]
+local channel = KEYS[3]
 local height = tonumber(ARGV[1])
 local width = tonumber(ARGV[2])
 
@@ -20,65 +22,65 @@ for i=1,height do
         local sum_r = 0
         local sum_g = 0
         local sum_b = 0
-        local live = redis.call("HEXISTS", prefix.."_GAMEOFLIFE_DATA", i..":"..j..":".."r")
+        local live = redis.call("HEXISTS", table, i..":"..j..":".."r")
 
         for k=1,8 do
             local x = i+neighbors[k][1]
             local y = j+neighbors[k][2]
             if x >= 1 and x<=height and y>=1 and y<=width then
-                local link_id = redis.call("HEXISTS", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."r")
+                local link_id = redis.call("HEXISTS", table, x..":"..y..":".."r")
                 if link_id == 1 then
                     count = count + 1
-                    sum_r = sum_r + redis.call("HGET", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."r")
-                    sum_g = sum_g + redis.call("HGET", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."g")
-                    sum_b = sum_b + redis.call("HGET", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."b")
+                    sum_r = sum_r + redis.call("HGET", table, x..":"..y..":".."r")
+                    sum_g = sum_g + redis.call("HGET", table, x..":"..y..":".."g")
+                    sum_b = sum_b + redis.call("HGET", table, x..":"..y..":".."b")
                 end
             end
         end
 
         if live == 0 then
             if count == 3 then
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", i)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", j)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", sum_r/count)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", sum_g/count)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", sum_b/count)
+                redis.call("RPUSH", queue, i)
+                redis.call("RPUSH", queue, j)
+                redis.call("RPUSH", queue, sum_r/count)
+                redis.call("RPUSH", queue, sum_g/count)
+                redis.call("RPUSH", queue, sum_b/count)
             end
         else
             if count < 2 then
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", i)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", j)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", -1)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", -1)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", -1)
+                redis.call("RPUSH", queue, i)
+                redis.call("RPUSH", queue, j)
+                redis.call("RPUSH", queue, -1)
+                redis.call("RPUSH", queue, -1)
+                redis.call("RPUSH", queue, -1)
             end
             if count > 3 then
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", i)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", j)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", -1)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", -1)
-                redis.call("RPUSH", prefix.."_GAMEOFLIFE_QUEUE", -1)
+                redis.call("RPUSH", queue, i)
+                redis.call("RPUSH", queue, j)
+                redis.call("RPUSH", queue, -1)
+                redis.call("RPUSH", queue, -1)
+                redis.call("RPUSH", queue, -1)
             end
         end
     end
 end
 
-while redis.call("LLEN", prefix.."_GAMEOFLIFE_QUEUE") > 0 do
-    local x = redis.call("LPOP", prefix.."_GAMEOFLIFE_QUEUE")
-    local y = redis.call("LPOP", prefix.."_GAMEOFLIFE_QUEUE")
-    local r = redis.call("LPOP", prefix.."_GAMEOFLIFE_QUEUE")
-    local g = redis.call("LPOP", prefix.."_GAMEOFLIFE_QUEUE")
-    local b = redis.call("LPOP", prefix.."_GAMEOFLIFE_QUEUE")
+while redis.call("LLEN", queue) > 0 do
+    local x = redis.call("LPOP", queue)
+    local y = redis.call("LPOP", queue)
+    local r = redis.call("LPOP", queue)
+    local g = redis.call("LPOP", queue)
+    local b = redis.call("LPOP", queue)
 
     if r == "-1" then
-        redis.call("HDEL", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."r")
-        redis.call("HDEL", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."g")
-        redis.call("HDEL", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."b")
+        redis.call("HDEL", table, x..":"..y..":".."r")
+        redis.call("HDEL", table, x..":"..y..":".."g")
+        redis.call("HDEL", table, x..":"..y..":".."b")
     else
-        redis.call("HSET", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."r", r)
-        redis.call("HSET", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."g", g)
-        redis.call("HSET", prefix.."_GAMEOFLIFE_DATA", x..":"..y..":".."b", b)
+        redis.call("HSET", table, x..":"..y..":".."r", r)
+        redis.call("HSET", table, x..":"..y..":".."g", g)
+        redis.call("HSET", table, x..":"..y..":".."b", b)
     end
 end
 
-redis.call("PUBLISH", prefix.."_GAMEOFLIFE_CHANNEL", 1)
+redis.call("PUBLISH", channel, 1)
--- services/GameOfLifeService.js
+++ services/GameOfLifeService.js
@@ -3,6 +3,10 @@ require('bluebird').promisifyAll(redis);
 const { EventEmitter } = require('events');
 const fs = require('fs')
 
+const TABLE_NAME = `${prefix}_GAMEOFLIFE_DATA`
+const QUEUE_NAME = `${prefix}_GAMEOFLIFE_QUEUE`
+const CHANNEL_NAME = `${prefix}_GAMEOFLIFE_CHANNEL`
+
 
 const client = redis.createClient(process.env.REDIS)
 const prefix = process.env.REDIS_PREFIX || ""
@@ -11,34 +15,26 @@ client.on("error", function (err) {
     console.log("Error " + err);
 });
 
-client.hdelAsync(`${prefix}_GAMEOFLIFE_DATA`, '112:-1:r').then(()=>{
-    return client.hdelAsync(`${prefix}_GAMEOFLIFE_DATA`, '112:-1:r')
-}).then(()=>{
-    return client.hdelAsync(`${prefix}_GAMEOFLIFE_DATA`, '112:-1:g')
-}).then(()=>{
-    return client.hdelAsync(`${prefix}_GAMEOFLIFE_DATA`, '112:-1:b')
-})
-
 exports.addPoints = (points, {
     r, g, b
-}) => points.reduce((acc, point) => acc.hmset(`${prefix}_GAMEOFLIFE_DATA`, {
+}) => points.reduce((acc, point) => acc.hmset(TABLE_NAME, {
     [`${point.x}:${point.y}:r`]: r,
     [`${point.x}:${point.y}:g`]: g,
     [`${point.x}:${point.y}:b`]: b
-}), client.multi()).publish(`${prefix}_GAMEOFLIFE_CHANNEL`, 1).execAsync()
+}), client.multi()).publish(CHANNEL_NAME, 1).execAsync()
 
-exports.iterate = (height, width) =>  client.evalAsync(fs.readFileSync('../lua/iterate.lua'), 1, prefix, height, width)
+exports.iterate = (height, width) =>  client.evalAsync(fs.readFileSync('../lua/iterate.lua'), 3, TABLE_NAME, QUEUE_NAME, CHANNEL_NAME, height, width)
 
-exports.getAll = () => client.hgetallAsync(`${prefix}_GAMEOFLIFE_DATA`)
+exports.getAll = () => client.hgetallAsync(TABLE_NAME)
 
 clientSubscriber = redis.createClient(process.env.REDIS)
 
-exports.initSubscriber = () => clientSubscriber.subscribe(`${prefix}_GAMEOFLIFE_CHANNEL`)
+exports.initSubscriber = () => clientSubscriber.subscribe(CHANNEL_NAME)
 
 exports.events = () => exports._event ? exports._event : exports._event = new EventEmitter
 
 clientSubscriber.on("message", (message) => {
-    if(message == `${prefix}_GAMEOFLIFE_CHANNEL`) {
+    if(message == CHANNEL_NAME) {
         exports.events().emit('data')
     }
 })
